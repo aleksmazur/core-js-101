87b4f59a6daef67b94044a456d0b709ed80f5acb
Aleksandra <aleksandra@MacBook-Air-Aleksandra.local>
2022-11-27 15:41:58 +0300

    feat: add numbers solutions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -109,9 +109,9 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-//   return Math.asin((y2 - y1) / (x2 - x1));
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const cos = (x1 * x2 + y1 * y2) / (Math.sqrt(x1 * x1 + y1 * y1) * Math.sqrt(x2 * x2 + y2 * y2));
+  return Math.acos(cos);
 }
 
 /**
@@ -202,8 +202,13 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n - 1; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -221,8 +226,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return (typeof Number(value) === 'number' && !Number.isNaN(Number(value))) ? +value : def;
 }
 
 module.exports = {
